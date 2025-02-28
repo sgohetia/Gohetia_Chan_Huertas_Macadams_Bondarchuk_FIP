6777e96d9c5443328b07f1281b8ce22e2535dab0
sheldongohetia <sheldongohetia.ca@gmail.com>
2025-02-28 01:34:00 -0500

    Updated Whole Page
    In this commit, added updates for home page, contact, soldiers letters page and other css  things also included updated logo for desktop and mobile some additional variation of hero images.

--- js/modules/contact_general.js
+++ js/modules/contact_general.js
@@ -61,4 +61,20 @@ export function contactgeneralform() {
   }
 
   form.addEventListener("submit", regForm);
+
+  //contact form
+  const tabs = document.querySelectorAll(".tab");
+  const forms = document.querySelectorAll(".form-container");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      tabs.forEach((t) => t.classList.remove("active"));
+      forms.forEach((f) => f.classList.remove("active"));
+
+      this.classList.add("active");
+      document
+        .querySelector(`#${this.getAttribute("data-target")}`)
+        .classList.add("active");
+    });
+  });
 }
